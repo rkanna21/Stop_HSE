8fe3f7ba7f7d184a683066b15662386c21131866
vagrant <vagrant@ironhack>
2013-12-09 11:50:31 +0000

    starting migrations for card

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131208111151) do
+ActiveRecord::Schema.define(version: 20131208170116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cards", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
